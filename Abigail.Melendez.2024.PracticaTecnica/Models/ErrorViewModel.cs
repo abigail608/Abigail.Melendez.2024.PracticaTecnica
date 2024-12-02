d6aa06e97cfcaa29cd8cfb8c64a671eb0dbfdf30
abigail608 <Abi06aguilar7@gmail.com>
2024-12-02 16:50:01 -0600

    Agregar archivos de proyecto.

---
+++ Abigail.Melendez.2024.PracticaTecnica/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Abigail.Melendez._2024.PracticaTecnica.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
